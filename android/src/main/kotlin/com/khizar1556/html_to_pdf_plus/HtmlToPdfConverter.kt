24ba53f2ee9e310e0808ff673ac24cd7194c1d05
ravilakhtariya <gunatitsolutions@gmail.com>
2024-12-13 21:03:23 +0530

    - margin added

--- android/src/main/kotlin/com/khizar1556/html_to_pdf_plus/HtmlToPdfConverter.kt
+++ android/src/main/kotlin/com/khizar1556/html_to_pdf_plus/HtmlToPdfConverter.kt
@@ -19,7 +19,7 @@ class HtmlToPdfConverter {
     }
 
     @SuppressLint("SetJavaScriptEnabled")
-    fun convert(filePath: String, applicationContext: Context, printSize: String, orientation: String, callback: Callback, margin: Map<String, Double>) {
+    fun convert(filePath: String, applicationContext: Context, printSize: String, orientation: String, margin: Map<String, Double>, callback: Callback) {
         val webView = WebView(applicationContext)
         val htmlContent = File(filePath).readText(Charsets.UTF_8)
         webView.settings.javaScriptEnabled = true
@@ -29,12 +29,12 @@ class HtmlToPdfConverter {
         webView.webViewClient = object : WebViewClient() {
             override fun onPageFinished(view: WebView, url: String) {
                 super.onPageFinished(view, url)
-                createPdfFromWebView(webView, applicationContext, printSize, orientation, callback, margin)
+                createPdfFromWebView(webView, applicationContext, printSize, orientation, margin, callback,)
             }
         }
     }
 
-    fun createPdfFromWebView(webView: WebView, applicationContext: Context, printSize: String, orientation: String, callback: Callback, margin: Map<String, Double>) {
+    fun createPdfFromWebView(webView: WebView, applicationContext: Context, printSize: String, orientation: String, margin: Map<String, Double>, callback: Callback) {
         val path = applicationContext.filesDir
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
             var mediaSize = PrintAttributes.MediaSize.ISO_A4
